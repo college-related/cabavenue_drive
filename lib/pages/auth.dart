eed43a939aa3a02ee1d61bb14d41f24dbfad08f7
Salipa-Gurung <salipagurung@gmail.com>
2022-06-28 16:06:59 +0545

    [WIP 1.0] : add login screen complete

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -20,7 +20,7 @@ class _AuthClassState extends State<AuthClass> {
 
   @override
   Widget build(BuildContext context) {
-    return isSignup? SignupScreen(callback: callback,):const LoginScreen();
+    return isSignup? SignupScreen(callback: callback,): LoginScreen(callback: callback,);
   }
 }
 
@@ -224,7 +224,8 @@ class SecondScreen extends StatelessWidget {
 }
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  const LoginScreen({Key? key,required this.callback}) : super(key: key);
+  final Function callback;
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
@@ -246,125 +247,78 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          margin:const EdgeInsets.symmetric( horizontal: 25),
+          margin:const EdgeInsets.symmetric(vertical: 8,horizontal: 20),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const Text('Login', style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),),
-              Container(
-                margin: const EdgeInsets.all(10),
-                child: RichText(
-                  text: const TextSpan(
-                    children: [
-                      TextSpan(text: 'Enter your Name, Email, Phone number, Documents photo and Password for sign up.', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300,color: Colors.grey)),
-                      TextSpan(text: 'Already have account?', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400,color: Colors.blue)),
-                    ]
+              const Image(image: AssetImage('assets/images/location-style-1-rounded.jpg')),
+              const Text('Cabavenue', style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500),),
+              const Text('Welcome back!'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Phone number',
+                    icon: Icon(Icons.call_rounded, color: Colors.blue,),
                   ),
                 ),
               ),
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        icon: Icon(Icons.account_circle_rounded, color: Colors.blue,),
-                        labelText: 'Enter your full name',
-                      ),
-                    ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    icon: Icon(Icons.lock_rounded, color: Colors.blue,),
+                    labelText: 'Enter password',
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        icon: Icon(Icons.email_rounded, color: Colors.blue,),
-                        labelText: 'Email',
-                      ),
-                    ),
+                ),
+              ),
+              Container(
+                margin:const EdgeInsets.symmetric(vertical: 30),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Colors.blue,
+                    shape:const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
+                    side: const BorderSide(color: Colors.blue),
+                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 80),
+                    elevation: 10,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Phone number',
-                        icon: Icon(Icons.call_rounded, color: Colors.blue,),
-                      ),
+                  onPressed: (){
+                    Navigator.of(context).pushNamed('/auth');
+                  },
+                  child: const Text('Login',style: TextStyle(fontSize: 20),),
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle:const TextStyle(fontSize: 15, color: Colors.blue),
+                ),
+                onPressed: () {},
+                child: const Text('Forgot Password?'),
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    const TextSpan(
+                      text: 'Don\'t have an account?',
+                      style: TextStyle(color: Colors.black54,fontSize: 16),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        icon: Icon(Icons.call_outlined, color: Colors.blue,),
-                        labelText: 'Secondary phone number',
-                      ),
+                    TextSpan(
+                      text: 'Register',
+                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400,color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          widget.callback();
+                        }
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        icon: Icon(Icons.location_on_rounded, color: Colors.blue,),
-                        labelText: 'Address',
-                      ),
-                    ),
-                  ),
-                  // Padding(
-                  //   padding: const EdgeInsets.all(8.0),
-                  //   child: TextFormField(
-                  //     decoration: const InputDecoration(
-                  //       border: UnderlineInputBorder(),
-                  //       icon: Icon(Icons.lock_rounded, color: Colors.blue,),
-                  //       labelText: 'Enter password',
-                  //     ),
-                  //   ),
-                  // ),
-                  // Padding(
-                  //   padding: const EdgeInsets.all(8.0),
-                  //   child: TextFormField(
-                  //     decoration: const InputDecoration(
-                  //       border: UnderlineInputBorder(),
-                  //       icon: Icon(Icons.lock_outline, color: Colors.blue,),
-                  //       labelText: 'Confirm password',
-                  //     ),
-                  //   ),
-                  // ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(vertical: 20),
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: SizedBox(
-                        width: 125,
-                        child: OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            primary: Colors.white,
-                            backgroundColor: Colors.blue,
-                            shape:const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
-                            side: const BorderSide(color: Colors.blue),
-                            padding: const EdgeInsets.symmetric(vertical: 8,horizontal: 30),
-                          ),
-                          onPressed: (){ },
-                          child: Row( 
-                            children:const [
-                              Text('Next',style: TextStyle(fontSize: 18),),
-                              Icon(Icons.arrow_forward, color: Colors.white,),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              )
+                  ]
+                )
+              ),
             ],
           ),
         ),
-      ),
+      )
     );
   }
 }
