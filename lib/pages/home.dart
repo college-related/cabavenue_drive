4c5bac2fdadbd18ba2fc5c966de7a4ef358c8d0c
Alson33 <garbuja12alson@gmail.com>
2022-07-06 10:56:08 +0545

    [refactor]: class name change, add SafeArea, etc

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,45 +1,77 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+import 'package:cabavenue_drive/services/auth_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  var user;
+
+  @override
+  void initState() {
+    super.initState();
+    _readUserData();
+  }
+
+  Future<void> _readUserData() async {
+    var u = await const FlutterSecureStorage().read(key: "CABAVENUE_USERDATA");
+    print(jsonEncode(u));
+    setState(() {
+      user = u;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Image(image: AssetImage('assets/images/location-style-1-rounded.jpg')),
-            const Text('Cabavenue', style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),),
-            // Text('Drive', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300, color: Colors.grey),),
-            SizedBox(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Image(
+              image: AssetImage('assets/images/location-style-1-rounded.jpg')),
+          const Text(
+            'Cabavenue',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
+          ),
+          Text(
+            '$user',
+          ),
+          // Text('Drive', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300, color: Colors.grey),),
+          SizedBox(
               // width: MediaQuery.of(context).size.width,
               child: Container(
-                margin:const EdgeInsets.symmetric(vertical: 30),
-                child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.blue,
-                    shape:const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
-                    side: const BorderSide(color: Colors.blue),
-                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 80),
-                    elevation: 10,
-                  ),
-                  onPressed: (){
-                    Navigator.of(context).pushNamed('/auth');
-                  },
-                  child: const Text('Signup',style: TextStyle(fontSize: 20),),),
-              )
-            )
-          ],
-        ),
-      )
-    );
+            margin: const EdgeInsets.symmetric(vertical: 30),
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.blue,
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8))),
+                side: const BorderSide(color: Colors.blue),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 80),
+                elevation: 10,
+              ),
+              onPressed: () {
+                AuthService().logout();
+                Navigator.of(context).pushNamed('/auth');
+              },
+              child: const Text(
+                'Log out',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ))
+        ],
+      ),
+    ));
   }
 }
