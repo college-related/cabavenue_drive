9f051e880acc35de79e1a659312aeb8205c8c685
Alson33 <garbuja12alson@gmail.com>
2022-07-06 10:54:29 +0545

    [screen]: add splash screen to check auth state of user and navigate accordingly

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final _storage = const FlutterSecureStorage();
+
+  @override
+  void initState() {
+    super.initState();
+    _readUserData().then(
+      (value) => {
+        if (value != null)
+          {Navigator.of(context).pushNamed('/home')}
+        else
+          {Navigator.of(context).pushNamed('/auth')}
+      },
+    );
+  }
+
+  Future _readUserData() async {
+    return await _storage.read(key: "CABAVENUE_USERDATA");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: const Color.fromARGB(255, 255, 255, 255),
+      child: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
