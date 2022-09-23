a3ef0b0d91b5e6c63bee56800b1eeb7575c25602
Alson33 <garbuja12alson@gmail.com>
2022-09-23 14:36:22 +0545

    [asset]: add free time illustration asset

--- lib/screens/subscreens/rides.dart
+++ lib/screens/subscreens/rides.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RideScreen extends StatefulWidget {
@@ -8,13 +10,43 @@ class RideScreen extends StatefulWidget {
 }
 
 class _RideScreenState extends State<RideScreen> {
+  final bool _hasRideRequests = Random().nextBool();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text('Rides'),
-        const Text('No Rides'),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 20.0),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 5.0),
+            child: Text('Today', style: Theme.of(context).textTheme.headline1),
+          ),
+          const Divider(
+            color: Colors.grey,
+          ),
+          !_hasRideRequests
+              ? Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/free-time.png',
+                      height: 300.0,
+                    ),
+                    Text(
+                      'No Requests',
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
+                    Text(
+                      'Enjoy the free time you don\'t have any ride request at the moment',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.subtitle2,
+                    ),
+                  ],
+                )
+              : const Text('Rides'),
+        ],
+      ),
     );
   }
 }
