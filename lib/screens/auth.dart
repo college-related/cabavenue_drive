b6209295f102bc42945e0535c04f358ff3d98a7c
Alson33 <garbuja12alson@gmail.com>
2022-09-24 15:17:21 +0545

    [fix]: fixes to the auth screens

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,10 +1,14 @@
+import 'dart:developer';
+
 import 'package:cabavenue_drive/helpers/snackbar.dart';
 import 'package:cabavenue_drive/services/auth_service.dart';
 import 'package:cabavenue_drive/widgets/auth/first_page.dart';
 import 'package:cabavenue_drive/widgets/auth/second_page.dart';
+import 'package:cabavenue_drive/widgets/auth/thrid_page.dart';
 import 'package:cabavenue_drive/widgets/custom_text_field.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:image_picker/image_picker.dart';
 
 class AuthPage extends StatefulWidget {
@@ -43,17 +47,18 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
-  bool isFirstScreen = true;
   final _signupFormKey = GlobalKey<FormState>();
   final TextEditingController _phoneController = TextEditingController();
-  final TextEditingController _secondaryphoneController =
-      TextEditingController();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _addressController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
       TextEditingController();
+  final TextEditingController _modelController = TextEditingController();
+  final TextEditingController _colorController = TextEditingController();
+  final TextEditingController _plateController = TextEditingController();
+
   final AuthService _authService = AuthService();
 
   XFile? citizenship;
@@ -62,6 +67,8 @@ class _SignupScreenState extends State<SignupScreen> {
   List<XFile?> images = [];
 
   final ImagePicker _imagePicker = ImagePicker();
+
+  int screenNumber = 1;
 
   void pickCitizen() async {
     XFile? citizenImage =
@@ -94,9 +101,11 @@ class _SignupScreenState extends State<SignupScreen> {
     _passwordController.dispose();
     _nameController.dispose();
     _phoneController.dispose();
-    _secondaryphoneController.dispose();
     _addressController.dispose();
     _confirmPasswordController.dispose();
+    _colorController.dispose();
+    _modelController.dispose();
+    _plateController.dispose();
   }
 
   void signup() {
@@ -105,9 +114,11 @@ class _SignupScreenState extends State<SignupScreen> {
         name: _nameController.text,
         email: _emailController.text,
         phone: _phoneController.text,
-        secondaryphone: _secondaryphoneController.text,
         address: _addressController.text,
         password: _passwordController.text,
+        color: _colorController.text,
+        model: _modelController.text,
+        plateNumber: _plateController.text,
         citizenship: citizenship,
         license: license,
         bluebook: bluebook,
@@ -125,40 +136,26 @@ class _SignupScreenState extends State<SignupScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
             child: Column(
               children: [
+                const Image(
+                    image: AssetImage(
+                        'assets/images/location-style-1-rounded.jpg')),
                 const Text(
-                  'Create Account',
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+                  'Cabavenue',
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500),
                 ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  child: RichText(
-                    text: TextSpan(children: [
-                      const TextSpan(
-                          text:
-                              'Enter your Name, Email, Phone number, Documents photo and Password for sign up.',
-                          style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w300,
-                              color: Colors.grey)),
-                      TextSpan(
-                          text: 'Already have account?',
-                          style: const TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.blue),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              widget.callback();
-                            }),
-                    ]),
-                  ),
+                const Text('Start of a new journey'),
+                GestureDetector(
+                  onTap: () {
+                    widget.callback();
+                  },
+                  child: const Text('Already have an account? Log in'),
                 ),
                 Form(
                   key: _signupFormKey,
-                  child: isFirstScreen
+                  child: screenNumber == 1
                       ? FirstPage(
                           nameController: _nameController,
                           emailController: _emailController,
@@ -166,83 +163,67 @@ class _SignupScreenState extends State<SignupScreen> {
                           phoneController: _phoneController,
                           confirmpasswordController: _confirmPasswordController,
                           addressController: _addressController,
-                          secondaryphoneController: _secondaryphoneController,
                         )
-                      : SecondScreen(
-                          pickCitizen: pickCitizen,
-                          pickLicense: pickLicense,
-                          pickBluebook: pickBluebook,
-                          citizenship: citizenship,
-                          bluebook: bluebook,
-                          license: license,
-                        ),
+                      : screenNumber == 2
+                          ? SecondScreen(
+                              colorContoller: _colorController,
+                              modelController: _modelController,
+                              plateController: _plateController,
+                            )
+                          : ThirdScreen(
+                              pickCitizen: pickCitizen,
+                              pickLicense: pickLicense,
+                              pickBluebook: pickBluebook,
+                              citizenship: citizenship,
+                              bluebook: bluebook,
+                              license: license,
+                            ),
                 ),
                 Container(
                   width: double.infinity,
                   margin: const EdgeInsets.symmetric(vertical: 20),
                   child: Row(
-                    mainAxisAlignment: isFirstScreen
+                    mainAxisAlignment: screenNumber == 1
                         ? MainAxisAlignment.end
                         : MainAxisAlignment.spaceBetween,
                     children: [
-                      SizedBox(
-                        width: 125,
-                        child: OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            primary: Colors.white,
-                            backgroundColor: Colors.white,
-                            shape: const RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(8))),
-                            side: const BorderSide(color: Colors.blue),
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 8, horizontal: 30),
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              isFirstScreen = !isFirstScreen;
-                            });
-                          },
-                          child: isFirstScreen
-                              ? Row(
-                                  children: const [
-                                    Text(
-                                      'Next',
-                                      style: TextStyle(
-                                          fontSize: 16, color: Colors.blue),
-                                    ),
-                                    Icon(
-                                      Icons.arrow_forward,
-                                      color: Colors.blue,
-                                    ),
-                                  ],
-                                )
-                              : Row(
-                                  children: const [
-                                    Icon(
-                                      Icons.arrow_back,
-                                      color: Colors.blue,
-                                    ),
-                                    Text(
-                                      'Back',
-                                      style: TextStyle(
-                                          fontSize: 16, color: Colors.blue),
-                                    ),
-                                  ],
-                                ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: screenNumber == 1
+                              ? Colors.teal
+                              : Colors.blueAccent,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(8))),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8, horizontal: 30),
                         ),
+                        onPressed: () {
+                          setState(() {
+                            if (screenNumber == 1) {
+                              screenNumber += 1;
+                            } else {
+                              screenNumber -= 1;
+                            }
+                          });
+                        },
+                        child: screenNumber == 1
+                            ? const Text(
+                                'Continue',
+                                style: TextStyle(fontSize: 16),
+                              )
+                            : const Text(
+                                'Back',
+                                style: TextStyle(fontSize: 16),
+                              ),
                       ),
-                      !isFirstScreen
-                          ? OutlinedButton(
-                              style: OutlinedButton.styleFrom(
-                                primary: Colors.white,
-                                backgroundColor: Colors.blue,
+                      screenNumber != 1
+                          ? ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.teal,
                                 shape: const RoundedRectangleBorder(
                                   borderRadius:
                                       BorderRadius.all(Radius.circular(8)),
-                                ),
-                                side: const BorderSide(
-                                  color: Colors.blue,
                                 ),
                                 padding: const EdgeInsets.symmetric(
                                   vertical: 8,
@@ -250,13 +231,19 @@ class _SignupScreenState extends State<SignupScreen> {
                                 ),
                               ),
                               onPressed: () {
-                                if (_signupFormKey.currentState!.validate()) {
-                                  signup();
+                                if (screenNumber == 3) {
+                                  if (_signupFormKey.currentState!.validate()) {
+                                    signup();
+                                  }
+                                } else {
+                                  setState(() {
+                                    screenNumber += 1;
+                                  });
                                 }
                               },
-                              child: const Text(
-                                'Sign up',
-                                style: TextStyle(fontSize: 20),
+                              child: Text(
+                                screenNumber == 3 ? 'Sign up' : 'Continue',
+                                style: const TextStyle(fontSize: 16),
                               ),
                             )
                           : Container(),
@@ -303,7 +290,7 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Form(
             key: _loginFormKey,
             child: Container(
-              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+              margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
               child: Column(
                 children: [
                   const Image(
@@ -313,15 +300,16 @@ class _LoginScreenState extends State<LoginScreen> {
                     'Cabavenue',
                     style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500),
                   ),
-                  const Text('Welcome back!'),
+                  const Text('Welcome back time for more rides!'),
+                  const SizedBox(height: 40.0),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: CustomTextField(
                       controller: _phonecontroller,
                       hintText: 'Phone number',
-                      icon: Icons.call_rounded,
+                      icon: Iconsax.call,
                       keyboardType: TextInputType.number,
-                      borderType: 'under;ine',
+                      borderType: 'full',
                     ),
                   ),
                   Padding(
@@ -329,31 +317,25 @@ class _LoginScreenState extends State<LoginScreen> {
                     child: CustomTextField(
                       controller: _passwordcontroller,
                       hintText: 'Password',
-                      icon: Icons.lock_rounded,
+                      icon: Iconsax.password_check,
                       isSecureText: true,
-                      borderType: 'underline',
+                      borderType: 'full',
                     ),
                   ),
                   Container(
-                    margin: const EdgeInsets.symmetric(vertical: 30),
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        primary: Colors.white,
-                        backgroundColor: Colors.blue,
-                        shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(8))),
-                        side: const BorderSide(color: Colors.blue),
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 12, horizontal: 80),
-                        elevation: 10,
+                    margin: const EdgeInsets.only(top: 30.0),
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: 50.0,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        elevation: MaterialStateProperty.all<double>(8.0),
                       ),
                       onPressed: () {
-                        login();
+                        if (_loginFormKey.currentState!.validate()) {
+                          login();
+                        }
                       },
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(fontSize: 20),
-                      ),
+                      child: const Text('Log in'),
                     ),
                   ),
                   TextButton(
@@ -367,7 +349,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   RichText(
                       text: TextSpan(children: [
                     const TextSpan(
-                      text: 'Don\'t have an account?',
+                      text: 'Don\'t have an account?  ',
                       style: TextStyle(color: Colors.black54, fontSize: 16),
                     ),
                     TextSpan(
