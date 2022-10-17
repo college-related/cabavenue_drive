f729222c12783180ffe8e0a14baa3ab53d6adad8
Alson33 <garbuja12alson@gmail.com>
2022-10-17 22:47:50 +0545

    [route]: add profile edit route and fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:cabavenue_drive/models/user_model.dart';
 import 'package:cabavenue_drive/providers/profile_provider.dart';
 import 'package:cabavenue_drive/screens/auth.dart';
+import 'package:cabavenue_drive/screens/editscreens/profile_edit.dart';
 import 'package:cabavenue_drive/screens/home.dart';
 import 'package:cabavenue_drive/screens/splash_screen.dart';
 import 'package:cabavenue_drive/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:provider/provider.dart';
 
 Future main() async {
@@ -14,27 +13,19 @@ Future main() async {
    * load .env file
    */
   await dotenv.load(fileName: ".env");
-
-  /**
-   * Add user data from localstorage to provider
-   */
-  var u = await const FlutterSecureStorage().read(key: "CABAVENUE_USERDATA");
-  UserModel user = await UserModel.deserialize(u ?? '{}');
-
-  runApp(MyApp(user: user));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key, required this.user}) : super(key: key);
-
-  final UserModel user;
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-            create: (context) => ProfileProvider(user: user)),
+          create: (context) => ProfileProvider(),
+        ),
       ],
       child: MaterialApp(
         title: 'Cabavenue Driver App',
@@ -44,6 +35,7 @@ class MyApp extends StatelessWidget {
           '/': (context) => const SplashScreen(),
           '/home': (context) => const HomePage(),
           '/auth': (context) => const AuthPage(),
+          '/profile-edit': (context) => const ProfileEditPage(),
         },
       ),
     );
